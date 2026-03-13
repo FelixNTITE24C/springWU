3c264e684e826a08e1e1554e6261a92fbcbb9069
FelixNTITE24C <felix.lindahl@elev.ga.ntig.se>
2026-03-13 14:14:04 +0100

    Vecka 11 Lektion
    hamburger menu
    tweaks
    section -> article

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-console.log("funkar!")
+function showMenu(){
+    let menu = document.querySelector("nav.menu");
+    menu.classList.toggle("show");
 
+    let layer = document.querySelector(".fade-layer");
+    layer.classList.toggle("visible")
+}
